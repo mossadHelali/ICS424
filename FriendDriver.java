c880e4af90a6ddec52ecd84a4a7536f74e2f8bff
Mohammed Alsayed <mohammed2250@gmail.com>
2016-12-31 17:00:49 +0300

    createFriends

--- FriendDriver.java
+++ FriendDriver.java
@@ -23,13 +23,30 @@ public class FriendDriver {
         }
     }
 
-    public static boolean createFriends(int id1, int id2){
+    public static boolean createFriends(int id1, int id2) throws SQLException{
+        String sql = "INSERT INTO FRIENDS VALUES (?, ?)";
 
+        // creating connection: this form of try is used to close resources after execution
+        try(Connection connection = DBUtil.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
+
+            statement.setInt(1, id1);
+            statement.setInt(2, id2);
+
+            int affected = statement.executeUpdate();
+
+            if(affected == 1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
 
     public static boolean populate1000rows(){
 
-        for
+
 
         return false;
     }
